2194655299293c324b790312d55ab13beaa833e4
yongsoocho <yongsoocho578@gmail.com>
2024-05-08 00:03:47 +0900

    next.config.mjs 바벨 설정

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ * @type {import('next').NextConfig}
+ * */
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  images: {
+    domains: ["i.postimg.cc"],
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
