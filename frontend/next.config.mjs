659197f132ee374dcf27e0c0313ed1908462cc74
yongsoocho <yongsoocho578@gmail.com>
2024-05-08 00:14:50 +0900

    SyntaxError: Class private methods are not enabled 수정

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,11 +3,6 @@
  * */
 const nextConfig = {
   output: "export",
-  reactStrictMode: true,
-  images: {
-    domains: ["i.postimg.cc"],
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
