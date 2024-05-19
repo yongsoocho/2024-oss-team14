8f99749752943166001abb08fa4974aa9c272aea
gytks4 <gytks4@naver.com>
2024-05-19 19:20:06 +0900

    draft

--- frontend/src/app/dashboard/components/main.tsx
+++ frontend/src/app/dashboard/components/main.tsx
@@ -4,6 +4,7 @@ import Search from "./search";
 import { Box, Flex, Separator } from "@radix-ui/themes";
 import SSRSafeSuspense from "@/components/SSRSafe";
 import DropdownFilter from "./DropdownFilter";
+import { CodeEditorWithDialog } from "./CodeEditor";
 
 export default function Main() {
   return (
@@ -17,6 +18,7 @@ export default function Main() {
       <SSRSafeSuspense fallback={null}>
         <div style={{ padding: "0px 40px" }}>
           <ReportedErrorList />
+          <CodeEditorWithDialog />
         </div>
       </SSRSafeSuspense>
     </Flex>
