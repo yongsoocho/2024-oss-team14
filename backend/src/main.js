fed781c932d6c7cd1e95a1a8d9d508f6f897bad0
gytks4 <gytks4@naver.com>
2024-06-02 14:49:38 +0900

    feat: cors 설정

--- backend/src/main.js
+++ backend/src/main.js
@@ -2,11 +2,30 @@ const express = require("express");
 const listEndpoints = require("express-list-endpoints");
 const { CustomError } = require("./pipe/error.js");
 const { Controller } = require("./routes/index.js");
+const cors = require("cors");
 
 const app = express();
 const PORT = 8000;
 
 /** Middlewares */
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://2024-oss-team14.pages.dev/",
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // 비허용된 요청은 차단
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
