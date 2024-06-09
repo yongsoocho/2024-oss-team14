272dd5f0b12c504e0215e7d885d155ab63ce9e81
yongsoocho <yongsoocho578@gmail.com>
2024-06-09 13:06:11 +0900

    fix resolve

--- backend/src/main.js
+++ backend/src/main.js
@@ -28,6 +28,10 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log(`Path: ${req.path}`);
+  console.log("Body:", req.body);
+});
 
 /** Routes */
 app.use("/", Controller());
